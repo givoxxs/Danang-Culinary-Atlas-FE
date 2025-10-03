edd9b7a5af901d7ace41aefe76ef9faf29c1c7f2
minhduc59 <hodangminhduc665@gmail.com>
2025-10-03 21:17:56 +0700

    Set up the structure and add fonts

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mulish: ['var(--font-mulish)', 'sans-serif'],
+        volkhov: ['var(--font-volkhov)', 'serif'],
+        'open-sans': ['var(--font-open-sans)', 'sans-serif'],
+        poppins: ['var(--font-poppins)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
